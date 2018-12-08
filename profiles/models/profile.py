2cb7264039b21191a323362995bf87332d21be2f
just-paja <just.paja@gmail.com>
2018-12-08 15:10:47 +0100

    Add slug to profiles

--- profiles/models/profile.py
+++ profiles/models/profile.py
@@ -1,9 +1,12 @@
-from django.db.models import CharField, ForeignKey, Model, TextField, PROTECT
+from django_extensions.db.fields import AutoSlugField
 from django_extensions.db.models import TimeStampedModel
+from django.db.models import CharField, ForeignKey, Model, TextField, PROTECT
+from django.utils.translation import ugettext_lazy as _
 
 
 class Profile(TimeStampedModel):
     name = CharField(max_length=127)
+    slug = AutoSlugField(_('Slug'), populate_from='name')
     about = TextField()
     group = ForeignKey(
         'ProfileGroup',
