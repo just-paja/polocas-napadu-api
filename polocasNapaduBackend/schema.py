b1560775fc7afd4f0fbf81bbf4bf29769b142758
just-paja <just.paja@gmail.com>
2018-12-22 13:29:49 +0100

    Add shows schema

--- polocasNapaduBackend/schema.py
+++ polocasNapaduBackend/schema.py
@@ -2,8 +2,14 @@ import graphene
 
 import bands.schema
 import locations.schema
+import shows.schema
 
-class Query(bands.schema.Query, locations.schema.Query, graphene.ObjectType):
+class Query(
+    bands.schema.Query,
+    locations.schema.Query,
+    shows.schema.Query,
+    graphene.ObjectType
+):
     pass
 
 schema = graphene.Schema(query=Query)
---
+++ shows/schema.py
@@ -0,0 +1,27 @@
+import graphene
+
+from graphene_django.types import DjangoObjectType
+
+from . import models
+
+class ShowType(DjangoObjectType):
+    class Meta:
+        model = models.Show
+
+class ShowTypeType(DjangoObjectType):
+    class Meta:
+        model = models.ShowType
+
+class ShowPhotoType(DjangoObjectType):
+    class Meta:
+        model = models.ShowPhoto
+
+class Query:
+    all_shows = graphene.List(ShowType)
+    all_show_types = graphene.List(ShowTypeType)
+
+    def resolve_all_shows(self, info, **kwargs):
+        return models.Show.objects.all()
+
+    def resolve_all_show_types(self, info, **kwargs):
+        return models.ShowTypes.objects.all()
