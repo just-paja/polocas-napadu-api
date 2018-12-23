9bc1cfd5cdb1adeb5f245c11b523f92f219a9bd9
just-paja <just.paja@gmail.com>
2018-12-23 02:55:58 +0100

    Add base for CI

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+
+from setuptools import find_packages, setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='polocas-napadu-api',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    description="API for improv group website",
+    long_description=README,
+    url='https://github.com/just-paja/polocas-napadu-api',
+    author='Pavel Žák',
+    author_email='pavel@zak.global',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
