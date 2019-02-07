63ddcbe216d143697b28b6a66e969d38389a26a0
just-paja <just.paja@gmail.com>
2019-02-08 00:02:12 +0100

    Redirect to external apps with API url

--- theatre_sports/views.py
+++ theatre_sports/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.conf import settings
 from django.shortcuts import redirect, get_object_or_404
 from django.contrib.admin.views.decorators import staff_member_required
@@ -10,16 +11,20 @@ from .models import Match
 @staff_member_required
 def match_control(request, match_id):
     match = get_object_or_404(Match, pk=match_id, show__visibility=VISIBILITY_PUBLIC)
+    api_url = request.build_absolute_uri(reverse('api_public'))
     token = get_token(request.user)
-    return redirect('%s?token=%s#/match/%s' % (
+    return redirect('%s?apiUrl=%s&token=%s#/match/%s' % (
         settings.APP_REFEREE_URL,
+        api_url,
         token,
         match.pk
     ))
 
 def match_scoreboard(request, match_id):
     match = get_object_or_404(Match, pk=match_id, show__visibility=VISIBILITY_PUBLIC)
-    return redirect('%s#/match/%s' % (
+    api_url = request.build_absolute_uri(reverse('api_public'))
+    return redirect('%s?apiUrl=%s#/match/%s' % (
         settings.APP_SCOREBOARD_URL,
+        api_url,
         match.pk,
     ))
