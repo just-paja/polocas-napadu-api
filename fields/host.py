d843e6e63209cc248c6572989736c4161c46f9d1
just-paja <just.paja@gmail.com>
2019-02-08 09:14:10 +0100

    Don't override host when already given

--- fields/host.py
+++ fields/host.py
@@ -1,6 +1,8 @@
 def append_host_from_context(url, context):
-    return '%s://%s%s' % (
-        'https' if context.is_secure() else 'http',
-        context.get_host(),
-        url
-    )
+    if '://' not in url:
+        return '%s://%s%s' % (
+            'https' if context.is_secure() else 'http',
+            context.get_host(),
+            url
+        )
+    return url
